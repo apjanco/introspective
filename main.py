cb1bc890546d36a45a988ebd0e115d8dcd0ec786
ajanco <apjanco@gmail.com>
2020-05-23 20:54:35 -0400

    moving from websocket to POST

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, WebSocket
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
-
+#i
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/{file}")
-async def root(request: Request, file: str):
-    code = (Path.cwd() / file).read_text()
+@app.get("/")
+async def root(request: Request):
+    code = (Path.cwd() / "main.py").read_text()
     return templates.TemplateResponse("index.html", {"request": request, "code": code})
+
+
+@app.post("/change")
+async def change_code(request: Request):
+    data = 'TODO'
+    (Path.cwd() / "main.py").write_text(data)
+
+
+
+
+
+
+
+
+
+
+
+
 
